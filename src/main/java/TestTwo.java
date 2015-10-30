75d30a49e28fa1996bacbad75f0f0291833bbd46
Garagylja <kein85@gmail.com>
2015-10-30 10:02:55 +0200

    Create unit test

--- src/main/java/TestTwo.java
+++ src/main/java/TestTwo.java
@@ -1,42 +1,49 @@
 import static java.lang.System.out;
 
 /**
- * Created by user on 29.10.2015.
+ * Created by Garagulya on 29.10.2015.
+ *
+ * 993 * 913  count of iterarion 681
+ Palindrome = 906609
  */
 public class TestTwo {
-
+/*We compare the first and poseledny sign including the second and penultimate, etc.
+ count - an ordinary counter. Find out how many pairs among each other are equal.
+  if the count is the number of elements that compare
+*
+* */
 	public static boolean isPalindrome (int num) {
-		int l = String.valueOf (num).length (); //берем длину числа, которое получили в результате умножения
+		int l = String.valueOf (num).length (); //take the length of which is obtained by multiplying
 		String strnumber = String.valueOf (num);
 		int k = 0, count = 0;
-		for (; k < l / 2; k++)                                               //стандартный цикл для палиндромов
+		for (; k < l / 2; k++)
 			if (strnumber.charAt (k) == strnumber.charAt (l - k - 1))
-				count++;     //сравниваем первые и поселедний знак в числе,
-		                     //второе и предпоследний и т.д.
-							//count - обычный счетчик. Узнаем, сколько пар в числе между собой равны.
-		if (count == k) {return true;}  //если count равен количеству элементов, которое сравнивали
+				count++;
+		if (count == k) {return true;}
 		return false;
 	}
-
+/*
+This method iterates through the possible values of a palindrome in two cycles of the maximum possible.
+To test calls the isPalindrome (int) that interrupts the cycle in the first match
+ */
 	public static int serchPalindrome () {
 		int res = 0;
 		int i,j,count=0;
-		for (i = 999 ; i > 900; i--) {// пере
+		for (i = 999 ; i > 900; i--) {
 			for ( j = 999;j>900;j--){
 			res = i * j;
 				count++;
 			if (isPalindrome (res)) {
-				out.println (res+" res | i "+i+" j |"+j+" count " + count);
+				out.println (" "+i+" * "+j+"  count of iterarion " + count+" ");
 				return res;}
 		}
 		}
-		System.out.println ();
 		return 0;
 	}
 
 
 
 	public static void main (String[] args) {
-		out.println (serchPalindrome ());
+		out.println ("Palindrome = " + serchPalindrome ());
 	}
 }
