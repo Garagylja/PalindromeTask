03d93bf396edd35fea427733639dd75cbb1958d5
Garagylja <kein85@gmail.com>
2015-10-30 11:21:21 +0200

    Оптимизировал до 674 итераций

--- src/main/java/TestTwo.java
+++ src/main/java/TestTwo.java
@@ -2,16 +2,17 @@ import static java.lang.System.out;
 
 /**
  * Created by Garagulya on 29.10.2015.
- *
+ * <p>
  * 993 * 913  count of iterarion 681
- Palindrome = 906609
+ * Palindrome = 906609
  */
 public class TestTwo {
-/*We compare the first and poseledny sign including the second and penultimate, etc.
- count - an ordinary counter. Find out how many pairs among each other are equal.
-  if the count is the number of elements that compare
-*
-* */
+
+	/*We compare the first and poseledny sign including the second and penultimate, etc.
+	 count - an ordinary counter. Find out how many pairs among each other are equal.
+	  if the count is the number of elements that compare
+	*
+	* */
 	public static boolean isPalindrome (int num) {
 		int l = String.valueOf (num).length (); //take the length of which is obtained by multiplying
 		String strnumber = String.valueOf (num);
@@ -22,25 +23,28 @@ public class TestTwo {
 		if (count == k) {return true;}
 		return false;
 	}
-/*
-This method iterates through the possible values of a palindrome in two cycles of the maximum possible.
-To test calls the isPalindrome (int) that interrupts the cycle in the first match
- */
+
+	/*
+	This method iterates through the possible values of a palindrome in two cycles of the maximum possible.
+	To test calls the isPalindrome (int) that interrupts the cycle in the first match
+	 */
 	public static int serchPalindrome () {
 		int res = 0;
-		int i,j,count=0;
-		for (i = 999 ; i > 900; i--) {
-			for ( j = 999;j>900;j--){
-			res = i * j;
-				count++;
-			if (isPalindrome (res)) {
-				out.println (" "+i+" * "+j+"  count of iterarion " + count+" ");
-				return res;}
-		}
+		int i, j, count = 0;
+		for (i = 999; i > 900; i--) {
+			for (j = 999; j > 900; j--) {
+				if (i != j) {
+					res = i * j;
+					count++;
+					if (isPalindrome (res)) {
+						out.println (" " + i + " * " + j + "  count of iterarion " + count + " ");
+						return res;
+					}
+				}
+			}
 		}
 		return 0;
 	}
-
 
 
 	public static void main (String[] args) {
--- src/test/java/TestTwoTest.java
+++ src/test/java/TestTwoTest.java
@@ -1,18 +1,20 @@
 import junit.framework.TestCase;
-import org.junit.Before;
-import org.junit.Test;
+		 import org.junit.Before;
+		 import org.junit.Test;
+
 /**
  * Created by user on 30.10.2015.
  */
 public class TestTwoTest extends junit.framework.TestCase {
-	int[] testPalindromeArr={12,99,989,133,55,44,878,90909,785,245,11};
-	boolean[] testBoolArr={false,true,true,false,true,true,true,true,false,false,true};
-	TestTwo palindrome =new TestTwo ();
 
-@Test
-	public void test_IsPalindrome()throws Exception{
-		for (int i=0;i<testPalindromeArr.length-1;i++){
-	assertEquals (testBoolArr[i],palindrome.isPalindrome (testPalindromeArr[i]));
+	int[] testPalindromeArr = {12, 99, 989, 133, 55, 44, 878, 90909, 785, 245, 11};
+	boolean[] testBoolArr = {false, true, true, false, true, true, true, true, false, false, true};
+	TestTwo palindrome = new TestTwo ();
+
+	@Test
+	public void test_IsPalindrome () throws Exception {
+		for (int i = 0; i < testPalindromeArr.length - 1; i++) {
+			assertEquals (testBoolArr[i], palindrome.isPalindrome (testPalindromeArr[i]));
 		}
-}
+	}
 }
